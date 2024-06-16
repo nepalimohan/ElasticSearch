799439db583c8278dbd0c38d8544a838ac25d955
Mohan Nepali <nepalimohan@outlook.com>
2024-06-16 21:24:31 +0545

    settings and automation part

---
+++ books/management/commands/insert_books.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand
+from books.models import Books
+import faker
+
+class Command(BaseCommand):
+    help = 'Insert 100 books into the database'
+
+    def handle(self, *args, **kwargs):
+        fake = faker.Faker()
+
+        books = []
+        for _ in range(400):
+            book = Books(
+                title=fake.sentence(nb_words=5),
+                author=fake.name(),
+                description=fake.text()
+            )
+            books.append(book)
+        
+        Books.objects.bulk_create(books)
+        self.stdout.write(self.style.SUCCESS('Successfully inserted 100 books'))
--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'books',
+    'django_elasticsearch_dsl',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+ELASTICSEARCH_DSL = {
+    'default': {
+        'hosts': 'http://localhost:9200'
     }
 }
 
